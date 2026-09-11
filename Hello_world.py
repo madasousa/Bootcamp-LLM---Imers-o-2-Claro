04ae0f27687691bc6d6e49c47b22e8b86e19f201
Mada Almeida <168130616+madasousa@users.noreply.github.com>
2026-09-10 21:50:34 -0300

    Add files via upload

---
+++ Hello_world.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+print("Aprendendo Python!")
+print("Meu nome é Madalena e eu sou de São Paulo!")
+minha_idade= 36
+meu_peso= 56
+minha_altura= 1.70
+print("minha_idade: ", minha_idade)
+print("meu_peso: ", meu_peso)
+print("minha_altura: ", minha_altura)
+
